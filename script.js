c9f3f9b6d228aa8201759b10dacb9f07a32dd64a
ansr91 <ansr91@gmail.com>
2016-05-18 22:05:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var myApp = angular.module("myAPP",['ngRoute']);
+
+myApp.config(function($routeProvider){
+  $routeProvider
+           .when('/home',{
+             templateUrl: 'home.html',
+             controller: 'studentCtrl'
+           })
+           .when('/add',{
+             templateUrl: 'add.html',
+             controller: 'studentCtrl'
+           })
+           .otherwise({
+             redirectTo: '/home'
+           });
+  });
+
+myApp.controller('studentCtrl',function(scope){
+  $scope.students =[];
+  $scope.student = {name:"",class:"",sex:"",mobile:"",email:""};
+
+  $scope.add = function(){
+    $scope.students.push($scope.student);
+    window.localStorage.setItem("students",JSON.stringify($scope.students));
+    alert(saved);
+    reset();
+  };
+
+function reset(){
+  $scope.student = {name:"",class:"",sex:"",mobile:"",email:""};
+}
+  
+});
