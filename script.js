d0969cbe08b521b40937e538fbbe0a62db9700e4
NagoorKaniS <nagoor.kanis@target.com>
2016-05-20 16:09:09 -0500

    working code

--- script.js
+++ script.js
@@ -1,38 +1,46 @@
-var myApp = angular.module("myApp",['ngRoute']);
+var myApp = angular.module("myApp", ['ngRoute']);
 
-myApp.config(function($routeProvider)  {
-  $routeProvider.
-           when('/home',{
-             templateUrl: 'templates/home.html',
-             controller: 'studentCtrl'
-           }).
-           when('/add',{
-             templateUrl: 'templates/add.html',
-             controller: 'formCtrl'
-           }).
-           otherwise({
-             redirectTo: '/home'
-           });
-  });
+myApp.config(function($routeProvider) {
+    $routeProvider.
+    when('/home', {
+        templateUrl: 'home.html',
+        controller: 'studentCtrl'
+    }).
+    when('/add', {
+        templateUrl: 'add.html',
+        controller: 'formCtrl'
+    }).
+    otherwise({
+        redirectTo: '/home'
+    });
+});
 
-myApp.controller('formCtrl',function(scope){
-  $scope.students =[];
-  $scope.student = {name:"",class:"",sex:"",mobile:"",email:""};
-
-  $scope.add = function(){
-    $scope.students.push($scope.student);
-    window.localStorage.setItem("students",JSON.stringify($scope.students));
-    alert(saved);
+myApp.controller('formCtrl', function($scope) {
+    $scope.students = [];
     reset();
-  };
 
-function reset(){
-  $scope.student = {name:"",class:"",sex:"",mobile:"",email:""};
-}
+    $scope.add = function() {
+        $scope.students.push($scope.student);
+        window.localStorage.setItem("students", JSON.stringify($scope.students));
+        alert('saved');
+        reset();
+    };
+
+    function reset() {
+        $scope.student = {
+            name: "",
+            class: "",
+            sex: "",
+            mobile: "",
+            email: ""
+        };
+				var students = window.localStorage.getItem("students");
+				$scope.students = JSON.parse(students);
+    }
 
 });
 
-myApp.controller('studentCtrl',function(scope){
-  var student = window.localStorage.getItem("students");
-  $scope.students = JSON.parse(student);
+myApp.controller('studentCtrl', function($scope) {
+    var students = window.localStorage.getItem("students");
+    $scope.students = JSON.parse(students);
 });
