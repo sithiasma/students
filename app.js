8ce0036c6c8b3dbaa78c55796b7d3476712e407c
ansr91 <ansr91@gmail.com>
2016-06-03 23:15:27 -0700

    see the view2.html it has some problem in bootstrap i can't put in the slideshow i dont know why please help me!!!

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ var myApp = angular.module("myApp", ['ngRoute']);
 
 myApp.config(function($routeProvider) {
     $routeProvider.
-    when('/view', {
-        templateUrl: 'view/view.html',
+    when('/view1', {
+        templateUrl: 'view/view1.html',
         controller: 'viewctrl'
     }).
     when('/add', {
         templateUrl: 'add/add.html',
         controller: 'addctrl'
     }).
+    when('/view2',{
+        templateUrl: 'view/view2.html',
+        controller: 'viewctrl'
+    }).
     otherwise({
-        redirectTo: '/view'
+        redirectTo: '/view1'
     });
 });
--- view/viewctrl.js
+++ view/viewctrl.js
@@ -1,6 +1,3 @@
-myApp.controller('viewctrl', function($scope, display) {
+myApp.controller('viewctrl', function($scope,display){
   $scope.students = display.getData();
-  var imgData = window.localStorage.getItem("photo") || "";
-
-  $scope.image = JSON.parse(imgData);
 });
