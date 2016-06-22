e6fce8605a28510153cec9f0e416060eb5c646d9
Sithi Asma <asmahhussain93@gmail.com>
2016-06-22 15:09:00 +0530

    no message

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ myApp.config(function($routeProvider) {
         templateUrl: 'view/view2.html',
         controller: 'viewctrl'
     }).
+    when('/show',{
+        templateUrl: 'show.html'
+    }).
+    when('/edit/:id',{
+        templateUrl: 'add/add.html',
+        controller: 'addctrl'
+    }).
     otherwise({
         redirectTo: '/view1'
     });
